3e7f60e7574928789f059da79172b2afb7751d43
adriancq07 <acq0712@gmail.com>
2020-10-09 00:16:49 -0600

    list

--- src/main.cpp
+++ src/main.cpp
@@ -17,65 +17,49 @@ int main() {
      ManagerPerson managerPerson;
 
 
-    Person  person1 = {"Mike", 30, 1};
-    /*  Persona persona2 = {"pep", 22, 43};
-      Persona persona3 = {"lucas", 33, 12};*/
+    Person person1 = {"Mike", 25, 1};
+    Person person2 = {"Ana", 25, 2};
+    Person person3 = {"Pedro", 20, 3};
 
-    managerPerson.addPerson(person1);}
+    managerPerson.addPerson(person1);
+    managerPerson.addPerson(person2);
+    managerPerson.addPerson(person3);
 
-//    // Convertir a JSON: Copia cada valor en un objeto JSON (SERIALIZACION)
-//    json jsonPerson;
-//    jsonPerson["nombre"] = person1->getNombre();
-//    jsonPerson["edad"] = person1->getEdad();
-//    jsonPerson["id"] = person1->getId();
-//
-//    string personaSerializado = jsonPerson.dump(4);
-//
-//    // Convertir desde JSON: copia cada valor desde un objeto de JSON (DESEREALIZAR)
-//    Person person22;
-//    person22.setNombre(jsonPerson["nombre"]);
-//    person22.setEdad(jsonPerson["edad"]);
-//    person22.setId(jsonPerson["id"]);
-//
-//    // Convertir de un String de JSON (DESEREALIZAR)
-//    string data = R"({
-//                        "nombre": "Mike",
-//                        "edad": 30,
-//                        "id" : 1
-//                    })";
-//
-//    json jsonData = json::parse(data);
-//    Person person33;
-//    person33.setNombre(jsonData["nombre"]);
-//    person33.setEdad(jsonData["edad"]);
-//    person33.setId(jsonData["id"]);
-//
-//
-//    //guarda el archivo
-//    ofstream archivo;
-//    try { archivo.open("ArchivoTexto.json", ios::binary); }
-//    catch (ifstream::failure a) {
-//        cout << "no se pudo abrir el archivo";
-//        exit(1);
-//    }
-//    //para la lista de personas poner:archivo << personasSerializando<< '\n';
-//    archivo << personaSerializado << '\n';
-//    archivo.close();
-//
-//    //lee el archivo
-//    ifstream archivo1;
-//    string texto;
-//    try { archivo1.open("ArchivoTexto.json", ios::binary); }
-//    catch (ifstream::failure a) {
-//        cout << "no se pudo abrir el archivo";
-//        exit(1);
-//    }
-//    cout << endl;
-//    while (!archivo1.eof()) {
-//        getline(archivo1, texto);
-//        cout << texto << endl;
-//    }
-//    archivo1.close();
-//
-//    return 0;
-//}
+    json jsonPersonas;
+
+    jsonPersonas["Personas"] = {{" Persona 1: ",person1.getNombre(),person1.getEdad(),person1.getId()},
+                                {" Persona 2: ",person2.getNombre(),person2.getEdad(),person2.getId()},
+                                {" Persona 3: ",person3.getNombre(),person3.getEdad(),person3.getId()}};
+
+    string personasSerializando = jsonPersonas.dump();
+
+    ofstream archivo;
+    try { archivo.open("ArchivoTexto.json", ios::binary); }
+    catch (ifstream::failure a) {
+        cout << "no se pudo abrir el archivo";
+        exit(1);
+    }
+    //para la lista de personas poner:archivo << personasSerializando<< '\n';
+    archivo << personasSerializando << '\n';
+    archivo.close();
+
+    //lee el archivo
+    ifstream archivo1;
+    string texto;
+    try { archivo1.open("ArchivoTexto.json", ios::binary); }
+    catch (ifstream::failure a) {
+        cout << "no se pudo abrir el archivo";
+        exit(1);
+    }
+    cout << endl;
+    while (!archivo1.eof()) {
+        getline(archivo1, texto);
+        cout << texto << endl;
+    }
+    archivo1.close();
+
+
+    return 0;
+
+}
+
